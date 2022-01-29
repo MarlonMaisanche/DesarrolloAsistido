d477d5ccd0bafa19884737e076f3f0b545906721
MarlonMaisanche <52811704+MarlonMaisanche@users.noreply.github.com>
2022-01-29 13:56:05 -0500

    Guardars Login
    Guardars Login

---
+++ AppTienda/src/app/guards/login.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+
+  constructor(private authService: AuthService,
+    private router: Router) {
+      
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) {
+
+      return this.authService.userData$.pipe(
+        map(user=>{
+          if(user){
+            this.router.navigateByUrl('')
+            return false
+          }
+          return true
+        })
+      )
+  }
+  
+}
